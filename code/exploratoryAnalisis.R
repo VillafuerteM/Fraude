3189f76415b8e21b3d28e5fb6d31b776d4722ddb
Mariano Villafuerte <villafuertem31@gmail.com>
2022-02-06 16:41:59 -0600

    Add files via upload

---
+++ code/exploratoryAnalisis.R
@@ -0,0 +1,11 @@
+########## ANALISIS EXPLORATORIO ##########
+# autor: 156057
+
+# librerias ----
+library(tidyverse)
+
+# lectura de datos ----
+datos = read.csv("D:/Tesis/Fraude/data/creditcard.csv")
+
+# summary ----
+summary(datos)
