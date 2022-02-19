2af9d1e77ae002916527846bbacb57a0c7bbffce
Mariano Villafuerte <villafuertem31@gmail.com>
2022-02-19 15:35:35 -0600

    Update exploratoryAnalisis.R

--- code/exploratoryAnalisis.R
+++ code/exploratoryAnalisis.R
@@ -1,5 +1,7 @@
-########## ANALISIS EXPLORATORIO ##########
-# autor: 156057
+################ DATOS NO BALANCEADOS ################
+#        DETECCION DE FRAUDE EN TRANSACCIONES        #
+#             PT1. ANALISIS EXPLORATORIO             #
+#                  by: VillafuerteM                  #
 
 # librerias ----
 library(tidyverse)
@@ -9,3 +11,9 @@ datos = read.csv("D:/Tesis/Fraude/data/creditcard.csv")
 
 # summary ----
 summary(datos)
+
+# analisis exploratorio ----
+# Fig $.$
+ggplot(datos, aes(x=Time, y=Amount, color=as.factor(Class))) + geom_point() + facet_grid(cols=vars(Class))
+
+# analisis de correlaciones ----
