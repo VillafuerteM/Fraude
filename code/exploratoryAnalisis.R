f57401bac81fd220d5dee1f7e7c2e96d73d742da
Mariano Villafuerte <villafuertem31@gmail.com>
2022-03-26 17:53:05 -0600

    Update exploratoryAnalisis.R

--- code/exploratoryAnalisis.R
+++ code/exploratoryAnalisis.R
@@ -12,7 +12,8 @@ library(tidymodels)
 
 # lectura de datos ----
 datos <- read.csv('creditcard.csv')
-summary(datos)
+# se hace un str para ver el tipo de variables y si identificamos faltantes
+str(datos)
 
 # ajuste de la variable Time ----
 # declaramos una fecha inicial aleatoria a falta de información
@@ -54,9 +55,215 @@ rm(entrenamiento2, prueba2, validacion2, fraudes, noFraudes, datos_particion)
 # analisis exploratorio ----
 # summary para tabla de medidas descriptivas
 summary(entrenamiento)
-# Fig $.$
-ggplot(data=entrenamiento) +
-  geom_histogram(aes(x=Amount, fill=factor(Class))) + 
-  facet_wrap(~Class,scales="free_y")
+
+# Histogramas ----
+ggplot(data=entrenamiento) + 
+  geom_histogram(aes(x=V1), bins=sqrt(180000)) +
+  coord_cartesian(xlim = c(-15, 3)) +
+  labs(y='Frecuencia') + theme_minimal()
+
+ggplot(data=entrenamiento) + 
+  geom_histogram(aes(x=Amount), bins=sqrt(180000)) +
+  coord_cartesian(xlim = c(0, 2500)) +
+  labs(y='Frecuencia') + theme_minimal()
+
+# Boxplots separados por clase ----
+# V1
+ggplot(data=entrenamiento,aes(y=V1, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-20,2.5)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V2 
+ggplot(data=entrenamiento,aes(y=V2, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-5,15)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V3 
+ggplot(data=entrenamiento,aes(y=V3, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-20,5)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V4 
+ggplot(data=entrenamiento,aes(y=V4, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  labs(x="Fraud") + theme_minimal()
+
+# V5 
+ggplot(data=entrenamiento,aes(y=V5, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-15,10)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V6 
+ggplot(data=entrenamiento,aes(y=V6, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-7.5,5)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V7 
+ggplot(data=entrenamiento,aes(y=V7, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-20,10)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V8 
+ggplot(data=entrenamiento,aes(y=V8, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-5,5)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V9 
+ggplot(data=entrenamiento,aes(y=V9, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-10,5)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V10
+ggplot(data=entrenamiento,aes(y=V10, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-15,4)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V11
+ggplot(data=entrenamiento,aes(y=V11, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  labs(x="Fraud") + theme_minimal()
+
+# V12
+ggplot(data=entrenamiento,aes(y=V12, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-20,5)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V13
+ggplot(data=entrenamiento,aes(y=V13, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-4,4)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V14
+ggplot(data=entrenamiento,aes(y=V14, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-20,5)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V15
+ggplot(data=entrenamiento,aes(y=V15, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-5,5)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V16
+ggplot(data=entrenamiento,aes(y=V16, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-15,5)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V17
+ggplot(data=entrenamiento,aes(y=V17, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  labs(x="Fraud") + theme_minimal()
+
+# V18
+ggplot(data=entrenamiento,aes(y=V18, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  labs(x="Fraud") + theme_minimal()
+
+# V19
+ggplot(data=entrenamiento,aes(y=V19, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  labs(x="Fraud") + theme_minimal()
+
+# V20
+ggplot(data=entrenamiento,aes(y=V20, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-2.5,2.5)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V21
+ggplot(data=entrenamiento,aes(y=V21, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-3,3)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V22
+ggplot(data=entrenamiento,aes(y=V22, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-3,3)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V23
+ggplot(data=entrenamiento,aes(y=V23, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-2.5,2.5)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V24
+ggplot(data=entrenamiento,aes(y=V24, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-2.5,2.5)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V25
+ggplot(data=entrenamiento,aes(y=V25, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-2,2)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V26
+ggplot(data=entrenamiento,aes(y=V26, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-2,2)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V27
+ggplot(data=entrenamiento,aes(y=V27, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-2,2)) +
+  labs(x="Fraud") + theme_minimal()
+
+# V28
+ggplot(data=entrenamiento,aes(y=V28, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(-1.5,1.5)) +
+  labs(x="Fraud") + theme_minimal()
+
+# Amount
+ggplot(data=entrenamiento,aes(y=Amount, x=factor(Class))) +
+  geom_boxplot() +
+  stat_summary(fun=mean,na.rm=T, geom="point", shape=20, color="red", fill="red", size=2)+
+  coord_cartesian(ylim=c(0,350)) +
+  labs(x="Fraud") + theme_minimal()
 
 # analisis de correlaciones ----
